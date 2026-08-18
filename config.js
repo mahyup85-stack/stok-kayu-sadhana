a9ca49cf7e1f11d3314f326fd1aa2f662e70742d
mahyup85-stack/test-san <mahyup85@gmail.com>
2026-08-18 14:36:22 +0800

    fix: sertakan config.js untuk deployment vercel

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://fcccuqnyxuwsrddlookt.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjY2N1cW55eHV3c3JkZGxvb2t0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4NDU2NzQsImV4cCI6MjA4NTQyMTY3NH0.w9p0yxWW1CtLm3Gj3uD1z3P1eWQxW_hB288iUwkfCd8'
+};
